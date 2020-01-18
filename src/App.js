a876ff5707c51503a0728eda965d34b055d4fe56
saul <43302778+saulwolfcode@users.noreply.github.com>
2020-01-18 17:22:18 -0300

    formas de cambiar el estado y omitir el .bind(), tener encuenta que existew una forma experimental llamados INICIALIZADORES DE PROPIEDAD ver donde es ta //// de esta forma imitimos el constructor y la palabra reservada this.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,84 @@
 import React, { Component,Fragment } from 'react';
+
+class Tasks extends Component {
+  constructor(props) {
+    super(props);
+    // this.agregar = this.agregar.bind(this);
+    // this.quitar = this.quitar.bind(this);
+    this.state = {
+      price: 0
+    };
+    const METHODS = ["agregar", "quitar", "limpiar"];
+
+    METHODS.forEach(method => {
+      this[method] = this[method].bind(this);
+    });
+  }
+  //////////////////////
+  //INICIALIZADORES
+  state = {
+    price: 0
+  };
+  agregar=()=> {
+    this.setState({
+      price: this.state.price + 1
+    });
+  }
+  quitar=()=> {
+    this.setState({
+      price: this.state.price - 1
+    });
+  }
+  limpiar=()=> {
+    this.setState({
+      price: 0
+    });
+  }
+
+  /////////////////////
+//   agregar() {
+//     this.setState({
+//       price: this.state.price + 1
+//     });
+//   }
+//   quitar() {
+//     this.setState({
+//       price: this.state.price - 1
+//     });
+//   }
+//   limpiar() {
+//     this.setState({
+//       price: 0
+//     });
+//   }
+  render() {
+    return (
+      <Fragment>
+        <div className="price">
+          <h1>{this.props.name}</h1>
+          <p>{this.state.price}</p>
+          <button onClick={this.agregar}>agregar</button>
+          <button onClick={this.quitar}>quitar</button>
+          <button onClick={this.limpiar}>limpiar</button>
+        </div>
+      </Fragment>
+    );
+  }
+}
+
 class App extends Component {
 	constructor(props) {
 		super(props);
-		this.state = {  }
+		this.state = {
+			
+		}
 	}
 	render() { 
 		return (
-		<Fragment>
-			<h1>app</h1>	
-		</Fragment>
-		);
+      <Fragment>
+		  <Tasks name={"price product"} price={10}/>
+      </Fragment>
+    );
 	}
 }
  
