ca9901a52f0c777d13f13b8ad7e76fc12021cc0a
saul <43302778+saulwolfcode@users.noreply.github.com>
2020-05-01 18:44:12 -0300

    comunicacion desde padre a hijo o hijo a padre por React.createRef()

--- src/components/ComunicacionInstancias.js
+++ src/components/ComunicacionInstancias.js
@@ -1,17 +1,65 @@
 
 import React, { Component } from 'react';
-class ComunicacionInstancias extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {  }
+
+const Header=()=>{
+    const subtitle={
+        fontWeight:"bold"
+    }
+    const title={
+        margin:"0.6rem",
+        border:"0.3em",
+        padding:"1px solid #d2d2d2",
+        fontFamily:"monospace",
+        fontSize:"17px"
+    }
+    return(
+        <header style={title}>
+            <h1>COMUNICACION ENTRE COMPONENTES</h1>
+            <div style={subtitle}>
+                Metodos de instancia
+            </div>
+        </header>
+    )
+}
+class Hijo extends Component {
+    state={
+        message:"HE MUTADO DESDE HACIENDO CLICK DESDE HIJO O PADRE"
+    }
+    dispachAlert=(e, message="Alert desde el HIJO")=>{
+        // alert(message)
+        this.setState({message})
     }
     render() { 
         return (
             <div>
-                
+                <h1>{this.state.message}</h1>
+                <button onClick={this.dispachAlert}>
+                    HIJO
+                </button>
             </div>
         );
     }
 }
- 
+
+class ComunicacionInstancias extends Component {
+
+        hijo= React.createRef()
+    
+    handleClick=()=>{
+       this.hijo.current.dispachAlert(null,"Click desde el PADRE")
+    }
+    render() {
+        
+        return (
+           <div>
+               <Header/>
+               <Hijo ref={this.hijo}/>
+               <button onClick={this.handleClick}>
+                   PADRE
+               </button>
+           </div> 
+        );
+    }
+}
+
 export default ComunicacionInstancias;
